3917058a6f0ed828d97cf7a2600cfb386456c34d
delamoar <you@example.com>
2023-08-23 19:55:02 +0300

    completed

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,27 @@
 def badge_maker(name):
-    return None
+    message = "Hello, my name is " + name + "."
+    return message
 
-def batch_badge_creator(names):
-    return None
+def batch_badge_creator(speaker_names):
+    badge_messages = [badge_maker(name) for name in speaker_names]
+    return badge_messages
 
-def assign_rooms(names):
-    return None
+def assign_rooms(speaker_names):
+    room_assignments = []
+    rooms = range(1, 8)  # Rooms 1 to 7
+    
+    for i, name in enumerate(speaker_names):
+        room_assignment = "Hello, " + name + "! You'll be assigned to room " + str(rooms[i]) + "!"
+        room_assignments.append(room_assignment)
+        
+    return room_assignments
 
-def printer(names):
-    return None
+def printer(speaker_names):
+    badge_messages = batch_badge_creator(speaker_names)
+    room_assignments = assign_rooms(speaker_names)
+    
+    for badge_message in badge_messages:
+        print(badge_message)
+    
+    for room_assignment in room_assignments:
+        print(room_assignment)
